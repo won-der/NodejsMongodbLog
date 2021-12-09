4d0f633599a17a7787d7283cdbe0cb2a73319e52
gjj <516715091@qq.com>
2021-12-08 17:33:06 -0800

    3rd commit: install mongoose and test

--- NodejsMongdbLog.js
+++ NodejsMongdbLog.js
@@ -1,7 +1,19 @@
 const express = require("express")
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test1');
+const schema={
+    data1:Number,
+    data2:Number
+}
+
+const Option = mongoose.model('Option', schema);
+
 const app = express()
-app.use('/',express.static('private'))
+app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
+    const a = new Option({data1:parseFloat(req.query.op1),
+        data2:parseFloat(req.query.op2)})
+    a.save()
     res.send(req.query)
     console.log(req.query)
 })
