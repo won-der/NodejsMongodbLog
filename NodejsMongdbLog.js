cd7889bae493fbd54779932d7e6d89a8e452f7f3
gjj <516715091@qq.com>
2021-12-12 17:36:41 -0800

    ejs is basicall success

--- NodejsMongdbLog.js
+++ NodejsMongdbLog.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test1');
+const ejs = require('ejs')
+mongoose.connect('mongo mongodb://172.21.2.236:27017/190110910309');
+//mongoose.connect('mongo mongodb://localhost');
 const schema={
-    data1:Number,
-    data2:Number
+    data1:String,
+    data2:String
 }
 
 const Option = mongoose.model('Option', schema);
@@ -11,9 +13,14 @@ const Option = mongoose.model('Option', schema);
 const app = express()
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    const a = new Option({data1:parseFloat(req.query.op1),
-        data2:parseFloat(req.query.op2)})
+    const a = new Option({data1:String(req.query.op1),
+        data2:String(req.query.op2)})
+    console.log(typeof(String(req.query.op2)))
+    console.log(String(req.query.op2))
     a.save()
+    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
+        res.send(str)
+    })
     res.send(req.query)
     console.log(req.query)
 })
